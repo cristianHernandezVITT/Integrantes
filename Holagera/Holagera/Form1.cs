ed9fe389ba903176f183e2bfa23ee285a5b59aa9
MoralesBilli <158227931+MoralesBilli@users.noreply.github.com>
2024-05-06 21:50:51 -0600

    Carpeta Programa en C# con raspberry

---
+++ Holagera/Holagera/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Security.Permissions;
+
+namespace Holagera
+{
+    public partial class Form1 : Form
+    {
+        SerialPort puertoar;
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            puertoar = new SerialPort();
+            puertoar.PortName = "COM6";
+            puertoar.BaudRate = 115200;
+            puertoar.DtrEnable = true;
+            try
+            {
+                puertoar.Open();
+                MessageBox.Show("Sí se conectó :)");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No está conectado" + ex);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                puertoar.WriteLine(textBox1.ToString());
+                MessageBox.Show("Se envio correctamente");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("No se envió" +ex);
+            }
+        }
+    }
+}
